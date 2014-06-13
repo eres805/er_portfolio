32cf865a62c7cdd4e758d47fa7d3493631cd397b
eres805 <eres805@gmail.com>
2014-06-13 04:06:31 -0400

    updated

--- er_portfolio_app.rb
+++ er_portfolio_app.rb
@@ -1,15 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
-require('pony')
+
 
 get('/') do
 	erb :index
-end
-
-post '/submit' do
-  Pony.mail :to => 'eres805@gmail.com',
-            :from => params[:email],
-            :name => params[:name],
-            :message => params[:message],
-            :subject => 'Test'
 end
